ea42c0341039479eaa93bd28ee7981ac94a0c589
LucasPas <7lpasiewicz@stpatrick.org>
2016-12-12 10:53:45 -0600

    Finished ITEM class, and I am 75% done with CART class.

---
+++ First-Semester-Final-master/cart.js
@@ -0,0 +1,22 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+  constructor(il,iq){
+    this.ItemList = il;
+    this.ItemQuantity = iq;
+  }
+  //Type the instance functions below this comment.
+  addItem(i,q){
+    this.ItemList.push(i);
+    this.ItemQuantity.push(q);
+  }
+  totalcart(){
+    let total = 0
+    for(var c=0; c<ItemList.lenght;c++;){
+      total= total+(this.ItemList[c]*this.ItemQuantity[c]);
+    }
+  }
+  subCart(d){
+    let subCart = new Cart()
+  }
+  }
+}
