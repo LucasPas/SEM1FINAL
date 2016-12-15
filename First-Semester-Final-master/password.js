bf32500ba95d338fc658daeb5e00dd16c1f003e5
LucasPas <7lpasiewicz@stpatrick.org>
2016-12-15 10:50:25 -0600

    Started Private Key

--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -6,14 +6,41 @@ class Password{
   }
   //Instance functions below this comment.
   validPublicKey(){
-    if(25>publickey.length>8){
+    while(25 >= publickey.length &&  publickey.length >= 8){
       return true
     }
-    else(){
+    else{
       return false
+    }
+  }
+  validPrivateKey(){
+    var dash = 0
+    var num = 0
+    let key= makePrivateKey();
+    while(this.privatekey[4]=="-" && this.privatekey[9]=="-"){
+        dash=true
+    }
+    for(i=0;i<=3;i++){
+      if(this.privatekey[i].isinteger() == 
+    }
     }
   }
 
 
   //Static function below this comment.
+  static makePrivateKey(){
+    let key = "";
+    var limit = 14
+    for(var x = 0; x<limit;x++){
+      if(x==4 || x==9){
+        key += "-"
+      }
+      else{
+        var number = Math.random()*10
+        number = Math.floor(number)
+        key += number
+      }
+    }
+    return key
+  }
 }
